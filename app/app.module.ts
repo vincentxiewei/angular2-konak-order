e39143e7b4c24e7276b8d8f583c9ea5924f116d7
Vincent Xie <vincentxiewei@gmail.com>
2016-08-30 09:47:28 +1000

    Halfway through routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,22 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
+import {HeroDetailComponent} from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
+import { routing } from './app.routing';
+import {DashboardComponent} from './dashboard.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
+  imports: [ BrowserModule, FormsModule, routing],
+  declarations: [ AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent],
+  providers: [HeroService],
+  bootstrap: [ AppComponent ],
+
 })
 export class AppModule { }
